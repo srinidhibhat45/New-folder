8960f680f2ba211b24e213f16c3a846e9e7e7036
srinidhibhat45 <srinidhibhat45@gmail.com>
2023-11-24 16:13:33 +0530

    sel2

--- 2/Hovering Event/src/WebTest.java
+++ 2/Hovering Event/src/WebTest.java
@@ -1,16 +1,17 @@
-import java.util.List;
+// import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.Color;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+// import org.openqa.selenium.support.ui.ExpectedConditions;
+// import org.openqa.selenium.support.ui.WebDriverWait;
 
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import org.testng.Assert.*;
+
+import org.testng.Assert;
 
 
 public class WebTest {
@@ -29,16 +30,58 @@ public class WebTest {
 	@BeforeTest
 	public void setUp() throws Exception {
 		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 
 	@Test
 	public void testWeb() throws Exception {
         
         	driver.get(baseUrl);
-        
-		// Fill your code here
-			
+    
+    String color1 = Color.fromString(driver.findElement(By.id("row1")).getCssValue("background-color")).asHex(); ;      
+    Assert.assertEquals(color1, "#000000");
+    row1Before = color1;
+
+    String color2 = Color.fromString(driver.findElement(By.id("row2")).getCssValue("background-color")).asHex(); ;      
+    Assert.assertEquals(color2, "#000000");
+    row2Before = color2;
+    
+    String color3 = Color.fromString(driver.findElement(By.id("row3")).getCssValue("background-color")).asHex(); ;      
+    Assert.assertEquals(color3, "#000000");
+    row3Before = color3;
+    
+    String color4 =  Color.fromString(driver.findElement(By.id("row4")).getCssValue("background-color")).asHex();      
+    Assert.assertEquals(color4, "#000000");
+    row4Before =color4;
+    
+    Actions act = new Actions(driver);
+    
+    act.moveToElement(driver.findElement(By.id("row1"))).build().perform();
+    
+    String color5 = Color.fromString(driver.findElement(By.id("row1")).getCssValue("background-color")).asHex(); ;      
+    Assert.assertEquals(color5, "#c8daf7");
+    row1After = color5;
+ 
+    act.moveToElement(driver.findElement(By.id("row2"))).build().perform();
+
+    
+    String color6 = Color.fromString(driver.findElement(By.id("row2")).getCssValue("background-color")).asHex(); ;      
+    Assert.assertEquals(color6, "#9df9ec");
+    row2After = color6;
+    
+    act.moveToElement(driver.findElement(By.id("row3"))).build().perform();
+    
+    String color7 = Color.fromString(driver.findElement(By.id("row3")).getCssValue("background-color")).asHex(); ;      
+    Assert.assertEquals(color7, "#b2a0e8");
+    row3After = color7;
+    
+    act.moveToElement(driver.findElement(By.id("row4"))).build().perform();
+
+    
+    String color8 = Color.fromString(driver.findElement(By.id("row4")).getCssValue("background-color")).asHex(); ;      
+    Assert.assertEquals(color8, "#d8a0e8");
+    row4After = color8;
+      
 	}
 
 	@AfterTest
@@ -47,3 +90,4 @@ public class WebTest {
 	}
 
 }
+
