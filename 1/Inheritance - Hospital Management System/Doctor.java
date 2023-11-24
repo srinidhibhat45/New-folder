128be7436257396a652381bd84e0052b8bb52d3b
guywithavan <151916224+guywithavan@users.noreply.github.com>
2023-11-24 14:28:07 +0530

    Update Doctor.java

--- 1/Inheritance - Hospital Management System/Doctor.java
+++ 1/Inheritance - Hospital Management System/Doctor.java
@@ -2,6 +2,13 @@
 public class Doctor extends User{
 	private String qualification;
     private int experience;
+
+	public Doctor(String name, String email, String mobileNumber, String address, String qualification, int experience) {
+		super(name, email, mobileNumber, address);
+		this.qualification = qualification;
+		this.experience = experience;
+	}
+
 	public String getQualification() {
 		return qualification;
 	}
@@ -18,5 +25,8 @@ public class Doctor extends User{
 	public void display()
     {
         //fill your code here
+		super.display(); // Call the display method of the parent class (User)
+		System.out.println("Qualification: " + qualification);
+		System.out.println("Experience: " + experience + " years");
     }
 }
