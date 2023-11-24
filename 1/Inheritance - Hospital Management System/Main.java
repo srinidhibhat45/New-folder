a0c35ef18d6e7bed62c84bb0248eca4e3836a683
guywithavan <151916224+guywithavan@users.noreply.github.com>
2023-11-24 14:28:39 +0530

    Update Main.java

--- 1/Inheritance - Hospital Management System/Main.java
+++ 1/Inheritance - Hospital Management System/Main.java
@@ -1,8 +1,47 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception{
 		//fill your code here
-	}
+		Scanner sc = new Scanner(System.in);
 
+		while (true) {
+			System.out.println("Menu");
+			System.out.println("1. Doctor");
+			System.out.println("2. Patient");
+			System.out.print("Enter your choice: ");
+
+			int choice = sc.nextInt();
+			sc.nextLine(); // consume new line
+
+			switch (choice) {
+				case 1:
+					System.out.println("Enter doctor details (name,email,mobile,address,qualification,experience):");
+					String doctorDetails = sc.nextLine();
+					String[] doctorInfo = doctorDetails.split(",");
+					Doctor doctor = new Doctor(
+							doctorInfo[0], doctorInfo[1], doctorInfo[2],
+							doctorInfo[3], doctorInfo[4], Integer.parseInt(doctorInfo[5]));
+
+					System.out.println("Doctor details");
+					doctor.display();
+					break;
+				case 2:
+					System.out.println("Enter patient details (name,email,mobile,address,bloodGroup,height,weight):");
+					String patientDetails = sc.nextLine();
+					String[] patientInfo = patientDetails.split(",");
+					Patient patient = new Patient(
+							patientInfo[0], patientInfo[1], patientInfo[2],
+							patientInfo[3], patientInfo[4], Double.parseDouble(patientInfo[5]),
+							Double.parseDouble(patientInfo[6])
+					);
+					System.out.println("Patient details");
+					patient.display();
+					break;
+				default:
+					System.out.println("Invalid input");
+			}
+		}
+	}
 }
